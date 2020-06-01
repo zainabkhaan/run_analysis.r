630a313c7bdd31a504cc7c32a8751b39076a8186
zainabkhaan <55424355+zainabkhaan@users.noreply.github.com>
2020-06-01 12:11:50 +0530

    Update run_analysis (2).R

--- run_analysis (2).R
+++ run_analysis (2).R
@@ -7,10 +7,6 @@ One of the most exciting areas in all of data science right now is wearable comp
 
 http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
-Here are the data for the project:
-
-https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
-
 You should create one R script called *run_analysis.R* that does the following.
 
 1. Merges the training and the test sets to create one data set.
